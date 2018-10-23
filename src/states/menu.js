4cc0b021b93614c1b72a1b666345cd7856464470
JaviBJ99 <javier.martinez127@gmail.com>
2018-10-23 16:53:59 +0200

    Update src/states/menu.js

--- src/states/menu.js
+++ src/states/menu.js
@@ -7,11 +7,11 @@ ProEvolutionQuidditch.menuState.prototype = {
     preload: function() {
         game.load.image('background','assets/images/background/background.png');
         game.load.image('harry','assets/images/harry.png' );
-        game.load.image('draco','..\assets\images\draco');
+        game.load.image('draco','../assets/images/draco');
         game.load.image('griffindor','assets/images/griffindor.png');
-        game.load.image('snitch','..\assets\images\snitch');
-        game.load.image('title','..\assets\images\text\title');
-        game.load.image('start','..\assets\images\text\start');
+        game.load.image('snitch','../assets/images/snitch');
+        game.load.image('title','/assets/images/text/title.png');
+        game.load.image('start','../assets/images/text/start');
         
     },
 
@@ -19,9 +19,10 @@ ProEvolutionQuidditch.menuState.prototype = {
        var backImage = game.add.sprite(0,0,'background');
        var harry = game.add.sprite(50,250,'harry');
        var harry2 = game.add.sprite(500,250,'harry');
+
        var griffindor = game.add.sprite(350, 250 , 'griffindor');
 
-       harry2.setAttribute('style','transform:rotate(180deg)');
+     
 
     
       
