8a7dd36ff07b0bdfc3ba5092b6b1a6acacbe89a1
cruzIago <iagocruzgarcia@gmail.com>
2018-10-23 16:27:14 +0200

    First mechanics

--- src/states/level.js
+++ src/states/level.js
@@ -1,18 +1,47 @@
 ProEvolutionQuidditch.levelState = function(game) {
 
 }
+    //Variables de control
+    var upKey;
+    var downKey;
+    var rightKey;
+    var downKey;
 
+    //Variables de objetos
+    var harry;
+    var snitch;
 ProEvolutionQuidditch.levelState.prototype = {
 
     preload: function() {
         
+        harry=game.add.sprite(40,40,'harry');
+        game.physics.enable(harry,Phaser.Physics.ARCADE);
+
+        snitch=game.add.sprite((Math.random()*(700-100)+100),(Math.random()*(500-100)+100),'snitch');
+        game.physics.enable(snitch,Phaser.Physics.ARCADE);
     },
 
     create: function() {
-
+        upKey=game.input.keyboard.addKey(Phaser.Keyboard.UP);
+        downKey=game.input.keyboard.addKey(Phaser.Keyboard.DOWN);
+        rightKey=game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
+        leftKey=game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
     },
 
     update: function() {
-
+        
+        if(upKey.isDown){
+            harry.y--;
+        }else if(downKey.isDown){
+            harry.y++;
+        }
+        if(rightKey.isDown){
+            harry.x++;
+        }else if(leftKey.isDown){
+            harry.x--;
+        }
+        
+        if(harry.check)
     }
+
 }
